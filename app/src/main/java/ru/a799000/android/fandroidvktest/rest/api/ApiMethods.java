d05558e04f6c41bff01a4205a7dc9f965a3b6d9b
Alexander <ag@799000.ru>
2017-11-19 14:47:41 +1000

    finished 7 19:34l

--- app/src/main/java/ru/a799000/android/fandroidvktest/rest/api/ApiMethods.java
+++ app/src/main/java/ru/a799000/android/fandroidvktest/rest/api/ApiMethods.java
@@ -4,7 +4,7 @@ package ru.a799000.android.fandroidvktest.rest.api;
  * Created by Alex on 18.08.2017.
  */
 
-public class ApiMethods {
+public class ApiMethods{
     public static final String WALL_GET = "wall.get";
     public static final String USERS_GET = "users.get";
     public static final String GROUPS_GET_MEMBERS = "groups.getMembers";
