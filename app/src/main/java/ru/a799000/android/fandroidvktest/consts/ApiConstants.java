c6ec4d7bcfe10651f4db06c0a71a4ea92f3f2e50
Alexander <ag@799000.ru>
2017-11-19 15:21:28 +1000

    finished 7 19:34l

--- app/src/main/java/ru/a799000/android/fandroidvktest/consts/ApiConstants.java
+++ app/src/main/java/ru/a799000/android/fandroidvktest/consts/ApiConstants.java
@@ -7,11 +7,19 @@ import com.vk.sdk.VKScope;
  */
 
 public class ApiConstants {
-    public static final String[] DEFAULT_LOGIN_SCOPE = {VKScope.EMAIL};
+
+    public static final String[] DEFAULT_LOGIN_SCOPE = {VKScope.AUDIO, VKScope.DIRECT, VKScope.VIDEO,
+            VKScope.WALL, VKScope.MESSAGES, VKScope.PHOTOS, VKScope.PAGES, VKScope.GROUPS, VKScope.EMAIL,
+            VKScope.STATS, VKScope.DOCS};
+
     public static final Double DEFAULT_VERSION = 5.67;
     public static final int DEFAULT_COUNT = 10;
     public static final String DEFAULT_USER_FIELDS = "photo_100";
     public static final String DEFAULT_MEMBER_FIELDS = "name,photo_100";
     public static final int MY_GROUP_ID = -86529522;//73476/86529522//154292319
     public static final String DEFAULT_GROUP_FIELDS = "status,description,site,links,contacts";
+
+    public static final String VIDEOS = "videos";
+    public static final String POSTS = "posts";
+    public static final String EXTENDED = "extended";
 }
